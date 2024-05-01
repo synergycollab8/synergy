8b2cbfe361b670104953448ca57bdd177cfaff2a
synergycollab8 <synergycollab8@gmail.com>
2024-05-01 13:43:18 +0000

    testupdate

--- scripts/network-down.sh
+++ scripts/network-down.sh
@@ -12,7 +12,7 @@
 export COMPOSE_PROJECT_NAME=net
 cd $HLF_NETWORK_DIR
 
-docker_files=" -f docker/docker-compose-ca.yaml -f docker/docker-compose-org.yaml"
+docker_files=" -f docker/docker-compose-ca.yaml -f docker/docker-compose-org.yaml -f docker/docker-compose-ipfs.yaml -f docker/docker-compose-db.yaml"
 if [ "${HLF_CA_ORDR_REQ}" = "Y" ]; then
 docker_files="$docker_files -f docker/docker-compose-orderer.yaml"
 fi
