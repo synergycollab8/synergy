535bcdc3b7e0d6c01e1284213cd7183ec309936d
synergycollab8 <166890124+synergycollab8@users.noreply.github.com>
2024-05-22 15:24:48 +0530

    Update server.js

--- mytest-network/chaincodeservicechat/server.js
+++ mytest-network/chaincodeservicechat/server.js
@@ -22,10 +22,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 const port = process.env.PORT || 6000;
 
 
-const chatServicerequest = require('./createChatServiceReq');
+const chatServicerequest = require('./createChatRequest');
 
 
-const getclientservicereq = require('./getChatServiceReq');
+//const getclientservicereq = require('./getChatServiceReq');
 
 const chatServiceData = require("./chatServiceData");
 
@@ -44,15 +44,18 @@ app.post('/createChat',async(req,res) => {
     //main("12345", "Issue", "BG","Subjecton Guarantee","Request for Guarantee product" );
     //room_name,room_id,userId,userName
     console.log(req.body.room_name,req.body.room_id,`create chatServiceRequest`);
-    const chatrequest = await chatServicerequest.chatRequestDetails('${req.body.room_id}','${req.body.room_name}','${req.body.userId}','${req.body.userName}');
+    const chatrequest = await chatServicerequest.chatRequestDetails(`${req.body.room_id}`,`${req.body.room_name}`,`${req.body.userId}`,`${req.body.userName}`,`$
+{req.body.message}`);
     console.log(chatrequest);
     console.log("chat request creation successful");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(chatrequest);
   }catch(error){
     res.status(500).send(err);
   }
 })
 
-app.get('/queryAllChatServiceData',async(req,res) => {
+/* app.get('/queryAllChatServiceData',async(req,res) => {
 
   try{
    console.log(`received call for querying allChatServiceData`);
@@ -77,6 +80,7 @@ app.get('/queryChatServiceData',async(req,res) => {
     res.status(500).send(error);
   }
 })
+*/
 
 //getChatServiceRequestdata(requestId)
 
@@ -86,8 +90,10 @@ app.post('/createChatHistory',async(req,res) => {
     //main("12345", "Issue", "BG","Subjecton Guarantee","Request for Guarantee product" );
     //room_id,userID,message,timestamp
     console.log(`received call for createChatHistory`);
-    const chathistorydata = await chatHistoryData.createChatHistory('${req.body.room_id}','${req.body.userID}','${req.body.message}',Date.now());
+    const chathistorydata = await chatHistoryData.createChatHistory(`${req.body.room_id}`,`${req.body.userID}`,`${req.body.message}`,Date.now());
     console.log(chathistorydata);
+    res.setHeader('Content-Type', 'application/json');
+    res.json(chathistorydata);
   }catch(error){
     res.status(500).send(err);
   }
@@ -108,15 +114,62 @@ app.get('/queryAllChatHistoryData',async(req,res) => {
 
 //app.get("/getAllChatServiceRequests",chatServiceData.getChatServiceRequest);
 //app.get("/getAllChatHistoryRequests",chatServiceData.getChatHistoryRequest);
-app.get("/getAllChatServiceRequests",chatServiceData.getAllChatServiceRequests);
-app.get("/getAllChatHistoryRequests",chatServiceData.getAllChatHistoryRequests);
+//app.get("/getAllChatServiceRequests",chatServiceData.getAllChatServiceRequests);
+//app.get("/getAllChatHistoryRequests",chatServiceData.getAllChatHistoryRequests);
 
-app.post('/getChatRequests',async(req,res) => {
+app.post('/getChatRequest',async(req,res) => {
 
   try{
     console.log('request received at chatrequests API:',req.body.room_id);
     console.log(`received req`);
     const reqdata = await getChatContactData.getChatRequest(`${req.body.room_id}`);
+    console.log(reqdata);
+    res.setHeader('Content-Type', 'application/json');
+    res.json(reqdata);
+  }catch(error){
+    console.log(error);
+    res.status(500).send(error);
+  }
+})
+
+
+app.post('/getUserChatRequest',async(req,res) => {
+
+  try{
+    console.log('request received at chatrequests API:',req.body.userId);
+    console.log(`received req`);
+    const reqdata = await getChatContactData.getUserChatRequest(`${req.body.userId}`);
+    console.log(reqdata);
+    res.setHeader('Content-Type', 'application/json');
+    res.json(reqdata);
+  }catch(error){
+    console.log(error);
+    res.status(500).send(error);
+  }
+})
+
+
+app.post('/getChathistory',async(req,res) => {
+
+  try{
+    console.log('request received at chatrequests API:',req.body.room_id);
+    console.log(`received req`);
+    const reqdata = await getChatContactData.getChathistory(`${req.body.room_id}`);
+    console.log(reqdata);
+    res.setHeader('Content-Type', 'application/json');
+    res.json(reqdata);
+  }catch(error){
+    console.log(error);
+    res.status(500).send(error);
+  }
+})
+
+app.post('/getUserChathistory',async(req,res) => {
+
+  try{
+    console.log('request received at userchatHistoryrequests API:',req.body.userId);
+    console.log(`received req`);
+    const reqdata = await getChatContactData.getUserChathistory(`${req.body.userId}`);
     console.log(reqdata);
     res.setHeader('Content-Type', 'application/json');
     res.json(reqdata);
